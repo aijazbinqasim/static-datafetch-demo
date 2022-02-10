6c9dacfac1d15cd7256c7f57150fbbca58c18737
aijazbinqasim <aijazbinqasim@hotmail.com>
2022-02-10 21:31:48 +0500

    notFound with fallback true

--- pages/[pid].js
+++ pages/[pid].js
@@ -4,6 +4,10 @@ import fs from "fs/promises";
 export default function Product(props) {
   const { product } = props;
 
+  if (!product) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
       <h1>{product.title}</h1>
@@ -12,13 +16,24 @@ export default function Product(props) {
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
-  const productId = params.pid;
+async function getResponse() {
   const filePath = path.join(process.cwd(), "data", "dumy-data.json");
   const json = await fs.readFile(filePath);
   const records = JSON.parse(json);
+
+  return records;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+
+  const records = await getResponse();
   const product = records.products.find((p) => p.id === productId);
+
+  if (!product) {
+    return { notFound: true };
+  }
 
   return {
     props: {
@@ -28,12 +43,17 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const records = await getResponse();
+  const ids = records.products.map((p) => p.id);
+  const params = ids.map((pid) => ({ params: { pid: pid } }));
+
   return {
-    paths: [
-      { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
-    ],
-    fallback: false,
+    paths: params,
+    // paths: [
+    //   { params: { pid: "p1" } },
+    //   // { params: { pid: "p2" } },
+    //   // { params: { pid: "p3" } },
+    // ],
+    fallback: true,
   };
 }
