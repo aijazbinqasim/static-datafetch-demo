82cd98df84d33ab3fb3e46975199f4e4393caf0a
aijazbinqasim <aijazbinqasim@hotmail.com>
2022-02-12 22:11:05 +0500

    client side data fetching & useSWR() HOOK

---
+++ pages/last-sales.js
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+export default function LastSales() {
+  const [sales, setSales] = useState([]);
+  //const [loading, setLoading] = useState(false);
+
+  const fetcher = (url) => fetch(url).then((r) => r.json());
+
+  const { data, error } = useSWR(
+    "https://static-datafetch-demo-default-rtdb.firebaseio.com/sales.json",
+    fetcher
+  );
+
+  useEffect(() => {
+    if (data) {
+      const salesArr = [];
+
+      for (const key in data) {
+        salesArr.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      setSales(salesArr);
+    }
+  }, [data]);
+
+  if (error) {
+    return <p>failed to load!</p>;
+  }
+  if (!data) {
+    return <p>loading...</p>;
+  }
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   fetch(
+  //     "https://static-datafetch-demo-default-rtdb.firebaseio.com/sales.json"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const salesArr = [];
+
+  //       for (const key in data) {
+  //         salesArr.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+
+  //       setSales(salesArr);
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+  // if (loading) {
+  //   return <p>Loading...</p>;
+  // }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
